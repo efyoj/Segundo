344f94e1f1117c561e0e5fa5dc2f28ff517e450f
Mathe <mathe@mathe-AS5552G.(none)>
2015-10-07 10:28:00 +0200

    ruby

---
+++ ruby/correcionsuma.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# correcionsuma.rb
+#Suma dos número introducidos por el usuario
+
+def suma(op1, op2)
+	op1.to_i + op2.to_i
+end
+
+def entrada(numero)
+	print "Operando #{numero.to_s}: "
+	gets.chomp
+end
+
+op1 = entrada(1)
+op2 = entrada(2)
+
+puts "#{ op1 } + #{ op2 } = #{ suma(op1, op2) }"
