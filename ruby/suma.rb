d4cca88dcf2c93854917fb1b80f92edab2252fe6
Mathe <mathe@mathe-AS5552G.(none)>
2015-10-07 10:07:25 +0200

    Ruby

---
+++ ruby/suma.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# suma.rb
+
+
+puts "Intoduce un numero"
+STDOUT.flush
+numero1 = gets.chomp
+numero1=numero1.to_i
+puts "Intoduce otro numero"
+STDOUT.flush
+numero2 = gets.chomp
+numero2=numero2.to_i
+puts numero1 + numero2
