d0708dc2b21a1fa5dfb78c203a3f1e3ee96b6810
Alvaro J. Mathe Alvaro <mathe_4_9@hotmail.com>
2015-10-21 09:50:21 +0200

    recordatorio

---
+++ Recordatorio/funciones.cpp
@@ -0,0 +1,6 @@
+#include "funciones.h"
+
+double sum(double op1, double op2) { return op1 + op2;}
+double res(double op1, double op2) { return op1 - op2;}
+double mul(double op1, double op2) { return op1 * op2;}
+double frac(double op1, double op2) { return op1 / op2;}
---
+++ Recordatorio/titulo.cpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "titulo.h"
+
+void titulo() {
+
+    system("toilet -f pagga -F border --gay 'calculadora'");
+
+}
